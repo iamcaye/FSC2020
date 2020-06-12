d09230bf06319015c4518d61a7568f056a570168
iamcaye <cayetano.biehler@gmail.es>
2020-06-12 16:49:37 +0200

    revisited ejercicio1.c

--- proyecto/ejercicio1.c
+++ proyecto/ejercicio1.c
@@ -17,62 +17,38 @@ struct InfoCliente{
 };
 
 int espera_evento(int sd, struct InfoCliente * array_clientes, int * array_listos, int * nueva_conexion){
-    fd_set conjunto_r;
     struct sockaddr_in * cli;
     socklen_t cli_len = sizeof(cli);
     int listos = sizeof(array_listos)/sizeof(array_listos[0]);
     int n_sd;
 
     nueva_conexion = 0;
-    
-
-    FD_ZERO(&conjunto_r);
 
     n_sd = accept(sd, (struct sockaddr *)&cli, &cli_len);
     if(n_sd < 0){
+        perror("accept");
         return -1;
     }
 
     unsigned i = 0;
     while(array_clientes[i].sd != -1){
-        if(array_clientes[i++].sd == n_sd){
+        if(array_clientes[i].sd == n_sd){
             printf("%d ya esta conectado!\n", n_sd);
             return -1;
         }
+        i++;
     }
     
     if((array_clientes[i-1].sd) == -1){
-        nueva_conexion = 1;
+        *nueva_conexion = 1;
+        char * num;
         array_listos[listos] = i-1;
-        array_clientes[i-1].fd = creat(strcat("cliente",i-1), 0666);
+        sprintf(num, "%d", i-1);
+        array_clientes[i-1].fd = creat(strcat("cliente",num), 0666);
         listos++;
     }
 
-//    unsigned j = 0;
-//    int maxfd = 0;
-//    for(j = 0 ; j < listos ; j++){
-//        FD_SET(array_clientes[array_listos[j]].sd, &conjunto);
-//        maxfd = max(maxfd, array_clientes[array_listos[j]].sd);
-//    }
-//
-//    int r = select(maxfd+1, &conjunto, sizeof(fd_set));
-//    if(r < 0){
-//        perror("select");
-//        exit(1);
-//    }else{
-//        for(j = 0; j < listos ; j++){
-//            if(FD_ISSET(array_clientes[array_listos[j]].sd), &conjunto){
-//            
-//            }
-//        }
-//    }
-
-    return &nueva_conexion;
+    return *nueva_conexion;
 }
 
-int max(int a, int b){
-    if(a < b){
-        return b;
-    }
-    return a;
-}
+int main(){return 0;}
